a24fa76850a99eab1dd384de30bc1a96aca01b2b
shiabsararrafid <shrafid.532@gmail.com>
2022-03-27 15:14:10 +0600

    header done

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>WATCH LOVER</h1>
+            <p >Choose Your Desired Watch At Best Price</p>
+        </div>
+    );
+};
+
+export default Header;
