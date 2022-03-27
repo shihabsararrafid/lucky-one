aed50bf94f572803c6ecd54edf2028b443b2b506
shiabsararrafid <shrafid.532@gmail.com>
2022-03-27 15:24:44 +0600

    shop created

---
+++ src/Component/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+
+    return (
+        <div className='shop-container'>
+            <div className="product-details">
+                This product
+                Details
+            </div>
+            <div className="cart-details">
+                This is cart details
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
